a13aca398a73d3640525d115ad66d21ef2642d16
haedoang <haedoang@gmail.com>
2022-05-02 17:21:49 +0900

    feat: index controller

--- src/main/java/kr/co/haedoang/sampleweb/Application.java
+++ src/main/java/kr/co/haedoang/sampleweb/Application.java
@@ -2,9 +2,24 @@ package kr.co.haedoang.sampleweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+    private final Environment env;
+
+    public Application(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping
+    public String index() {
+        return "hello, this mode is " + env.getProperty("mode");
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
